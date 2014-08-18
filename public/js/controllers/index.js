b6260dcc0c38217eeb115f9e21937d9cf14d53dc
Ji Ho Koo <jihokoo@gmail.com>
2014-08-17 21:47:44 -0400

    add form and styling

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,4 +1,7 @@
 angular.module('paypal.controllers.index', [])
   .controller('IndexController', ['$scope', '$http', '$state', 'Global', function($scope, $http, $state, Global){
     $scope.global = Global;
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'MM/dd/yyyy', 'shortDate'];
+  $scope.format = $scope.formats[3];
   }]);
