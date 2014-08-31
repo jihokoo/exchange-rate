7748570fea4d05085aef6fc094699e7df009eeee
Ji Ho Koo <jihokoo@gmail.com>
2014-08-30 22:14:40 -0400

    change styling of select box and set default option to USD

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,5 @@
 var data = require('../models/store');
-var forex = require('../../forex/forex')();
+var forex = require('../../forex/forex')({loadFromFile: true});
 
 exports.create = function(req, res){
   var newData;
@@ -21,37 +21,40 @@ exports.getAll = function(req, res){
 };
 
 exports.convert = function(req, res){
+  console.log(req.body);
   var convertTo = req.body.convertTo;
   var amount = parseFloat(req.body.amount);
   if(convertTo && (amount >= 0)){
     forex.getRate(convertTo)
       .then(function(data){
+        console.log(data);
         var symbol = data.split(' ')[0];
-        var rate = parseFloat(data.split(' ')[1]);
+        var rate = (Math.round(parseFloat(data.split(' ')[1]) + 0.00001) * 100) / 100;
         amount = rate * amount;
-        res.json(200, {amount: symbol+' '+amount});
+        res.json(200, {symbol: symbol, amount: amount});
       }, function(err){
-        console.log('conver', err);
+        console.log('err', err)
+        console.log('convert', err);
         res.json(500, err);
       });
   } else{
+    console.log('here')
     res.json(500, {message: 'Error: must post a currency code to convert to and a valid amount'})
   }
 };
 
 exports.getConversionRate = function(req, res){
-  console.log('getConversionRate')
-  console.log(req.params.currency)
   var currency = req.params.currency;
   if(currency){
     forex.getRate(currency)
       .then(function(data){
-        res.json(200, {rate: data})
+        var symbol = data.split(' ')[0];
+        var rate = (Math.round(parseFloat(data.split(' ')[1]) + 0.00001) * 100) / 100;
+        res.json(200, {rate: rate, symbol: symbol})
       }, function(err){
         res.json(500, {message: 'Error: '+err.message});
       });
   } else{
-    console.log('hello')
     res.json(500, {message: 'Error: must enter a currency code'});
   }
 };
--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -2,72 +2,85 @@ angular.module('paypal.controllers.index', [])
   .controller('IndexController', ['$scope', '$http', '$state', 'Global', function($scope, $http, $state, Global){
     $scope.global = Global;
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'MM/dd/yyyy', 'shortDate'];
-  $scope.format = $scope.formats[3];
-  $scope.transaction = {};
-  $scope.transactions = [];
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'MM/dd/yyyy', 'shortDate'];
+    $scope.format = $scope.formats[3];
+    $scope.transaction = {};
+    $scope.transactions = [];
 
-  (function(){
-    $http.get('/paypal/activity')
-    .success(function(data){
-      data.forEach(function(transaction){
-        transaction.amount = '$ ' + transaction.amount
-        $scope.transactions.push(transaction);
-      });
-    })
-    .error(function(err){
-      console.log('get error', err)
-    });
-  })();
+    $scope.currencyShortList = [
+      {display: 'USD $', code: 'USD'},
+      {display: 'INR ₹', code: 'INR'},
+      {display: 'CAD $', code: 'CAD'},
+      {display: 'CNY CN¥', code: 'CNY'},
+      {display: 'EUR €', code: 'EUR'}
+    ];
 
-  // i can do something here with $scope.$watch('select') maybe....
-  // i catually don't want to do that
+    // i can do something here with $scope.$watch('select') maybe....
+    // i catually don't want to do that
 
-  $scope.format = function(date){
-    var d = new Date(date);
-    var date = d.getDate() + 1;
-    var month = d.getMonth() + 1;
-    var year = d.getFullYear();
-    var formattedString = month + '/' + date + '/' + year;
-    return formattedString;
-  };
+    $scope.format = function(date){
+      var d = new Date(date);
+      var date = d.getDate() + 1;
+      var month = d.getMonth() + 1;
+      var year = d.getFullYear();
+      var formattedString = month + '/' + date + '/' + year;
+      return formattedString;
+    };
 
-  $scope.create = function(){
-    $http.post('/paypal/create', {
-      type: $scope.transaction.type,
-      subject: $scope.transaction.subject,
-      date: $scope.transaction.date,
-      amount: $scope.transaction.amount
-    })
-    .success(function(data){
-      console.log(typeof data.date)
-      $scope.transactions.push(data);
-    })
-    .error(function(err){
-      console.log('create error', err)
-    });
-  };
+    $scope.create = function(){
+      $http.post('/paypal/create', {
+        type: $scope.transaction.type,
+        subject: $scope.transaction.subject,
+        date: $scope.transaction.date,
+        amount: $scope.transaction.amount
+      })
+        .success(function(data){
+          data.currency = $scope.currencyShortList[0];
+          $scope.transactions.push(data);
+        })
+        .error(function(err){
+          console.log('create error', err);
+        });
+    };
+
+    $scope.convert = function(transaction){
+      console.log(transaction)
+      $http.post('/paypal/currencyConversion', {
+        convertTo: transaction.currency, 
+        amount: transaction.amount
+      })
+        .success(function(data){
+          console.log(data.amount);
+          transaction.amount = data.amount;
+        })
+        .error(function(err){
+          console.log('convert error', err)
+        });
+    };
 
 
-  $scope.convert = function(){
-    $http.post('/paypal/currencyConversion')
+    $scope.getConversionRate = function(currencyCode){
+      $http.get('/paypal/conversionRate/'+currencyCode)
+        .success(function(data){
+          console.log('conversionRate', data);
+        })
+        .error(function(err){
+          console.log('conversion error', err);
+        });
+    };
+
+    (function(){
+      $http.get('/paypal/activity')
       .success(function(data){
-        console.log('currencyConversion', data)
+        data.forEach(function(transaction){
+          transaction.currency = $scope.currencyShortList[0].code;
+          console.log(transaction);
+          $scope.transactions.push(transaction);
+        });
       })
       .error(function(err){
-        console.log('convert error', err)
+        console.log('get error', err)
       });
-  };
-
-
-  $scope.getConversionRate = function(){
-    $http.get('/paypal/conversionRate/')
-      .success(function(data){
-        console.log('conversionRate', data)
-      })
-      .error(function(err){
-        console.log('conversion error', err)
-      });
-  };
+    })();
 
   }]);
